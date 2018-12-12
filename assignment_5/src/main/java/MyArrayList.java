365229017bd5c8763d198ae210fbd4f1d91dd95f
Gabriele Di Rosa <dirosg@usi.ch>
2018-12-12 09:50:54 +0100

    non static methods

--- assignment_5/src/main/java/MyArrayList.java
+++ assignment_5/src/main/java/MyArrayList.java
@@ -4,11 +4,9 @@ import java.util.*;
 
 public class MyArrayList<E> extends AbstractList<E> implements Contract, List<E>, RandomAccess, Cloneable, java.io.Serializable{
     private final java.util.ArrayList<E> list;
-    private static int size = 0;
 
     public MyArrayList(java.util.ArrayList<E> list) {
         this.list = list;
-        size = list.size();
     }
 
     @Requires("availableElement")
@@ -18,12 +16,12 @@ public class MyArrayList<E> extends AbstractList<E> implements Contract, List<E>
 
     @Override
     public int size() {
-        return size;
+        return list.size();
     }
 
     @Pure
-    private static boolean availableElement(int index) {
-        return size > index;
+    private boolean availableElement(int index) {
+        return this.list.size() > index;
     }
-    
+
 }
