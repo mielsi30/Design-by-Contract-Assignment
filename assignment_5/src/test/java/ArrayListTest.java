73a16a977e118dd9ec381c9525fde2b63280b912
Jacopo Fidacaro <jacopo.fidacaro@finconsgroup.com>
2018-12-12 17:57:07 +0100

    Minor test changes

--- assignment_5/src/test/java/ArrayListTest.java
+++ assignment_5/src/test/java/ArrayListTest.java
@@ -3,28 +3,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.ArrayList;
 
-public class ArrayListTest {
-
+class ArrayListTest {
 
     @Test
-    public void getOnEmptyArray() throws Throwable {
-        MyArrayList myArray = new MyArrayList<String>(new ArrayList());
+    void getOnEmptyArray() {
+        MyArrayList<String> myArray = new MyArrayList<>(new ArrayList<>());
         assertThrows(Contract.PreconditionViolation.class, () -> myArray.get(0));
     }
 
     @Test
-    public void addWrongElement() throws Throwable {
-        MyArrayList myArray = new MyArrayList<String>(new ArrayList());
+    void addWrongElement() {
+        MyArrayList<String> myArray = new MyArrayList<>(new ArrayList<>());
         assertThrows(Contract.PreconditionViolation.class, () -> myArray.add(1, "1"));
     }
 
     @Test
-    public void clearList() throws Throwable {
-        MyArrayList myArray = new MyArrayList<String>(new ArrayList());
+    void clearList() {
+        MyArrayList<String> myArray = new MyArrayList<>(new ArrayList<>());
         assertThrows(Contract.PreconditionViolation.class, () -> myArray.add(0, "1"));
     }
-
-
-
-
 }
