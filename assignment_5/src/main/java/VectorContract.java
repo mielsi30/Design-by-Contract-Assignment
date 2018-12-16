827bef69098abe08e6f8a692a277f651e75caacd
Jacopo Fidacaro <jacopo.fidacaro@finconsgroup.com>
2018-12-16 16:39:55 +0100

    Add sizeStaysTheSame postcondition contract for vector.

--- assignment_5/src/main/java/VectorContract.java
+++ assignment_5/src/main/java/VectorContract.java
@@ -12,18 +12,22 @@ public interface VectorContract<E> extends Contract {
     int indexOf(Object o);
 
     @Requires("nonNegativeIndex")
+    @Ensures("sizeStaysTheSame")
     int indexOf(Object o, int index);
 
     int lastIndexOf(Object o);
 
     @Requires("indexWithinBounds")
+    @Ensures("sizeStaysTheSame")
     int lastIndexOf(Object o, int index);
     E elementAt(int index);
 
     @Requires("nonEmptyVector")
+    @Ensures("sizeStaysTheSame")
     E firstElement();
 
     @Requires("nonEmptyVector")
+    @Ensures("sizeStaysTheSame")
     E lastElement();
 
     void setElementAt(E obj, int index);
@@ -54,5 +58,10 @@ public interface VectorContract<E> extends Contract {
     @Pure
     default boolean indexWithinBounds(int index){
         return index < size();
+    }
+
+    @Pure
+    default boolean sizeStaysTheSame() {
+        return this.size() == Contract.old(this).size();
     }
 }
